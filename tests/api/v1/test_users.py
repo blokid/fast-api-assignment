02712880edf160bb11c1b545452b0273ce032225
Win Htet Aung <whaung1998@gmail.com>
2025-02-17 19:17:47 +0700

    test[org]: add tests for organization crud

--- tests/api/v1/test_users.py
+++ tests/api/v1/test_users.py
@@ -12,21 +12,8 @@ from starlette.status import (
     HTTP_404_NOT_FOUND,
 )
 
-from app.core import settings, token
-from app.core.constant import (
-    FAIL_VALIDATION_MATCHED_USER_EMAIL,
-    FAIL_VALIDATION_MATCHED_USER_ID,
-    FAIL_VALIDATION_USER_DUPLICATED,
-    FAIL_VALIDATION_USER_WRONG_PASSWORD,
-    SUCCESS_DELETE_USER,
-    SUCCESS_GET_USERS,
-    SUCCESS_MATCHED_USER_ID,
-    SUCCESS_MATCHED_USER_TOKEN,
-    SUCCESS_SIGN_IN,
-    SUCCESS_UPDATE_USER,
-    SUCCESS_VERIFICATION_EMAIL,
-    SUCCESS_VERIFY_USER,
-)
+from app.core import constant, settings, token
+from app.schemas.token import TokenInvite
 from app.schemas.user import UserBase, VerificationTokenData
 
 environ["APP_ENV"] = "test"
@@ -42,7 +29,7 @@ async def test_signup(
 ) -> None:
     response = await client.post(app.url_path_for("auth:signup"), json=random_user)
     result = response.json()
-    assert result.get("message") == SUCCESS_VERIFICATION_EMAIL
+    assert result.get("message") == constant.SUCCESS_VERIFICATION_EMAIL
     assert result.get("data") == {}
     assert response.status_code == HTTP_201_CREATED
 
@@ -54,7 +41,7 @@ async def test_signup_duplicate_user(
     result = response.json()
     assert response.status_code == HTTP_400_BAD_REQUEST
     assert result.get("app_exception") == "Response4XX"
-    assert result["context"].get("reason") == FAIL_VALIDATION_USER_DUPLICATED
+    assert result["context"].get("reason") == constant.FAIL_VALIDATION_USER_DUPLICATED
 
 
 async def test_verification_email(
@@ -70,7 +57,7 @@ async def test_verification_email(
     result = response.json()
     created_random_user["id"] = result.get("data").get("id")
     assert response.status_code == HTTP_200_OK
-    assert result["message"] == SUCCESS_VERIFY_USER
+    assert result["message"] == constant.SUCCESS_VERIFY_USER
 
 
 async def test_signin_error(
@@ -85,7 +72,9 @@ async def test_signin_error(
 
     assert response.status_code == HTTP_400_BAD_REQUEST
     assert result.get("app_exception") == "Response4XX"
-    assert result["context"].get("reason") == FAIL_VALIDATION_MATCHED_USER_EMAIL
+    assert (
+        result["context"].get("reason") == constant.FAIL_VALIDATION_MATCHED_USER_EMAIL
+    )
 
     # FAIL_VALIDATION_USER_WRONG_PASSWORD
     invalid_user["email"] = created_random_user["email"]
@@ -94,7 +83,9 @@ async def test_signin_error(
 
     assert response.status_code == HTTP_400_BAD_REQUEST
     assert result.get("app_exception") == "Response4XX"
-    assert result["context"].get("reason") == FAIL_VALIDATION_USER_WRONG_PASSWORD
+    assert (
+        result["context"].get("reason") == constant.FAIL_VALIDATION_USER_WRONG_PASSWORD
+    )
 
 
 async def test_signin(
@@ -107,7 +98,7 @@ async def test_signin(
     token = result.get("data").get("token")
 
     assert response.status_code == HTTP_200_OK
-    assert result.get("message") == SUCCESS_SIGN_IN
+    assert result.get("message") == constant.SUCCESS_SIGN_IN
     assert token.get("access_token")
     assert token.get("token_type") == settings.jwt_token_prefix
 
@@ -127,10 +118,27 @@ async def test_auth_info(
     result_user = result.get("data")
 
     assert response.status_code == HTTP_200_OK
-    assert result.get("message") == SUCCESS_MATCHED_USER_TOKEN
+    assert result.get("message") == constant.SUCCESS_MATCHED_USER_TOKEN
     assert result_user.get("id") == created_random_user.get("id")
     assert result_user.get("username") == created_random_user.get("username")
     assert result_user.get("email") == created_random_user.get("email")
+
+
+async def test_auto_created_org(
+    app: FastAPI, client: AsyncClient, created_random_user: dict[str, str]
+) -> None:
+    headers = {
+        "Authorization": f"{settings.jwt_token_prefix} {created_random_user.get('token').get('access_token')}",
+        **client.headers,
+    }
+    response = await client.get(
+        app.url_path_for("user:organizations"),
+        headers=headers,
+    )
+    result = response.json()
+    assert result["message"] == constant.SUCCESS_GET_USER_ORGANIZATION
+    assert len(result["data"]["organizations"]) == 1
+    assert response.status_code == HTTP_200_OK
 
 
 async def test_all_user(app: FastAPI, client: AsyncClient) -> None:
@@ -138,7 +146,7 @@ async def test_all_user(app: FastAPI, client: AsyncClient) -> None:
 
     result = response.json()
     assert response.status_code == HTTP_200_OK
-    assert result.get("message") == SUCCESS_GET_USERS
+    assert result.get("message") == constant.SUCCESS_GET_USERS
     assert isinstance(result.get("data"), list)
 
 
@@ -156,7 +164,7 @@ async def test_user_by_id(
 
     result = response.json()
     assert response.status_code == HTTP_200_OK
-    assert result.get("message") == SUCCESS_MATCHED_USER_ID
+    assert result.get("message") == constant.SUCCESS_MATCHED_USER_ID
     assert result.get("data").get("id") == created_random_user.get("id")
     assert result.get("data").get("username") == created_random_user.get("username")
     assert result.get("data").get("email") == created_random_user.get("email")
@@ -177,7 +185,7 @@ async def test_user_by_id_error(
     result = response.json()
     assert response.status_code == HTTP_404_NOT_FOUND
     assert result.get("app_exception") == "Response4XX"
-    assert result["context"].get("reason") == FAIL_VALIDATION_MATCHED_USER_ID
+    assert result["context"].get("reason") == constant.FAIL_VALIDATION_MATCHED_USER_ID
 
 
 async def test_update_user(
@@ -200,10 +208,127 @@ async def test_update_user(
 
     result = response.json()
     assert response.status_code == HTTP_200_OK
-    assert result.get("message") == SUCCESS_UPDATE_USER
+    assert result.get("message") == constant.SUCCESS_UPDATE_USER
     assert result.get("data").get("id") == update_user.get("id")
     assert result.get("data").get("username") == update_user.get("username")
     assert result.get("data").get("email") == update_user.get("email")
+
+
+async def test_create_organization(
+    app: FastAPI,
+    client: AsyncClient,
+    created_random_user: dict[str, str],
+    random_org: dict[str, str],
+    created_random_org: dict[str, str],
+) -> None:
+    headers = {
+        "Authorization": f"{settings.jwt_token_prefix} {created_random_user.get('token').get('access_token')}",
+        **client.headers,
+    }
+    response = await client.post(
+        app.url_path_for("organization:create"),
+        json=random_org,
+        headers=headers,
+    )
+    result = response.json()
+    assert response.status_code == HTTP_201_CREATED
+    assert result.get("message") == constant.SUCCESS_CREATE_ORGANIZATION
+    assert result.get("data").get("name") == random_org.get("name")
+    created_random_org.update(result.get("data"))
+
+
+async def test_update_organization(
+    app: FastAPI,
+    client: AsyncClient,
+    created_random_user: dict[str, str],
+    random_org: dict[str, str],
+    created_random_org: dict[str, str],
+) -> None:
+    headers = {
+        "Authorization": f"{settings.jwt_token_prefix} {created_random_user.get('token').get('access_token')}",
+        **client.headers,
+    }
+    random_org["name"] = "new org name"
+    response = await client.patch(
+        app.url_path_for(
+            "organization:update", organization_id=created_random_org["id"]
+        ),
+        json=random_org,
+        headers=headers,
+    )
+    result = response.json()
+    assert response.status_code == HTTP_200_OK
+    assert result.get("message") == constant.SUCCESS_UPDATE_ORGANIZATION
+    assert result.get("data").get("name") == random_org.get("name")
+    created_random_org.update(result.get("data"))
+
+
+async def test_organization_invite(
+    app: FastAPI,
+    client: AsyncClient,
+    created_random_user: dict[str, str],
+    random_invite: dict[str, str],
+    created_random_org: dict[str, str],
+) -> None:
+    headers = {
+        "Authorization": f"{settings.jwt_token_prefix} {created_random_user.get('token').get('access_token')}",
+        **client.headers,
+    }
+    response = await client.post(
+        app.url_path_for(
+            "organization:invite", organization_id=created_random_org["id"]
+        ),
+        json=random_invite,
+        headers=headers,
+    )
+    result = response.json()
+    assert response.status_code == HTTP_200_OK
+    assert result.get("message") == constant.SUCCESS_INVITATION_EMAIL
+    created_random_org.update(result.get("data"))
+
+
+async def test_accept_organization_invitation(
+    app: FastAPI,
+    client: AsyncClient,
+    random_invite: dict[str, str],
+    created_random_org: dict[str, str],
+) -> None:
+    created_token = token.create_org_invitation_token(
+        invite=TokenInvite(
+            organization_id=created_random_org["id"], email=random_invite["email"]
+        ),
+        secret_key=settings.secret_key.get_secret_value(),
+    )
+    response = await client.post(
+        app.url_path_for("organization:accept-invite"),
+        json={"token": created_token.token},
+    )
+    result = response.json()
+    assert response.status_code == HTTP_404_NOT_FOUND
+    assert result.get("context").get("reason") == constant.FAIL_NEED_TO_SIGN_UP
+
+
+async def test_delete_organization(
+    app: FastAPI,
+    client: AsyncClient,
+    created_random_user: dict[str, str],
+    created_random_org: dict[str, str],
+) -> None:
+    headers = {
+        "Authorization": f"{settings.jwt_token_prefix} {created_random_user.get('token').get('access_token')}",
+        **client.headers,
+    }
+    response = await client.delete(
+        app.url_path_for(
+            "organization:update", organization_id=created_random_org["id"]
+        ),
+        headers=headers,
+    )
+    result = response.json()
+    assert response.status_code == HTTP_200_OK
+    assert result.get("message") == constant.SUCCESS_DELETE_ORGANIZATION
+    assert result.get("data").get("deleted_at") is not None
+    created_random_org.update(result.get("data"))
 
 
 async def test_delete_user(
@@ -219,4 +344,4 @@ async def test_delete_user(
 
     result = response.json()
     assert response.status_code == HTTP_200_OK
-    assert result.get("message") == SUCCESS_DELETE_USER
+    assert result.get("message") == constant.SUCCESS_DELETE_USER
